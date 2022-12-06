aa0ecbc80559393e7564347beffc89a945cb278f
crissegura00 <crisemanuelsegura@gmail.com>
2022-12-06 12:15:43 -0300

    cambios

--- src/componentes/PlanCanje.jsx
+++ src/componentes/PlanCanje.jsx
@@ -3,11 +3,18 @@
 const PlanCanje = ( ) => {
 
     return(
-        <div  style={{padding:'10px', textAlign:'center'}}>
+        <div  style={{padding:'10px 15px', textAlign:'center'}}>
             <h1>Plan canje</h1>
             <p> ¿Querés cambiar tu iPhone y entregar el tuyo como parte de pago? <br />
-            En Apple-Shop tenemos la solución. Es tan simple como traer tu iPhone <br />
+            En <b>Apple-Shop</b> tenemos la solución. Es tan simple como traer tu iPhone <br />
             (con cargador), elegir un nuevo dispositivo y abonar la diferencia. </p>
+            <h3>Requisitos para el plan canje:</h3>
+            <p> <b>Estéticos:</b> El dispositivo no tiene que tener la pantalla rota ni rallones en la parte trasera.
+            Se aceptan unicamente leves desgastes por el uso del mismo. </p>
+            <p> <b>Funcionamiento:</b> Lo unico que se acepta que no funcione es la huella (pero si debe andar el home) o el face id.
+            La bateria tiene que estar entre el 100% y el 85%, no menos. </p>
+            <h3>¿Qué modelos entran en el plan canje?</h3>
+            <p> Tomamos iPhone a partir del 7. </p>
         </div>
     )
 }
