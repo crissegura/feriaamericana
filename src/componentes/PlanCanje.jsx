68ae204e6ef165d03b7ca80881a9cbc712586d20
crissegura00 <crisemanuelsegura@gmail.com>
2022-12-06 12:03:59 -0300

    cambios

--- src/componentes/PlanCanje.jsx
+++ src/componentes/PlanCanje.jsx
@@ -3,9 +3,12 @@
 const PlanCanje = ( ) => {
 
     return(
-        <>
-        <h1>Plan canje</h1>
-        </>
+        <div  style={{padding:'10px', textAlign:'center'}}>
+            <h1>Plan canje</h1>
+            <p> ¿Querés cambiar tu iPhone y entregar el tuyo como parte de pago? <br />
+            En Apple-Shop tenemos la solución. Es tan simple como traer tu iPhone <br />
+            (con cargador), elegir un nuevo dispositivo y abonar la diferencia. </p>
+        </div>
     )
 }
 
